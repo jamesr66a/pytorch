4b7f6dcbb4786751ab4d7079cb2291f5ad141eb3
James Reed <jamesreed@fb.com>
2018-02-23 09:51:36 -0800

    Specialization pass for Python functions

--- torch/csrc/jit/script/compiler.h
+++ torch/csrc/jit/script/compiler.h
@@ -1,16 +1,20 @@
 #pragma once
+#include <functional>
 #include <memory>
 #include <string>
 
 #include "torch/csrc/jit/ir.h"
+#include "torch/csrc/jit/pybind.h"
 
 namespace torch {
 namespace jit {
 namespace script {
 
+using ResolutionCallback = std::function<py::function(Graph*, std::string)>;
+
 struct CompilationUnitImpl;
 struct CompilationUnit {
-  CompilationUnit();
+  CompilationUnit(ResolutionCallback rcb);
   void define(const std::string& str);
   std::shared_ptr<Graph> getGraph(const std::string& func_name);
   ~CompilationUnit();
@@ -19,7 +23,9 @@ struct CompilationUnit {
   std::unique_ptr<CompilationUnitImpl> pImpl;
 };
 
-std::unique_ptr<CompilationUnit> jitScriptCompile(const std::string& script);
+std::unique_ptr<CompilationUnit> jitScriptCompile(
+    const std::string& script,
+    ResolutionCallback rcb);
 
 } // namespace script
 } // namespace jit
